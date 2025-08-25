da3587f841b62c672c61f6251fdd85d701acdf20
jeetghelani61 <jeetghelani61@gmail.com>
2025-08-25 11:11:54 +0530

    Create p4.dart

---
+++ p4.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+void main(){
+
+
+  print("enter the value of r:");
+  var r=int.parse(stdin.readLineSync().toString());
+
+  var cricle=13.14*r*r;
+  print("cricel value is:$cricle");
+}
