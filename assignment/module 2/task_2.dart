b9bbd1856f00c3b29ef157ec4efe1934a1c86f88
jeetghelani61 <jeetghelani61@gmail.com>
2025-08-25 11:58:48 +0530

    Update task_2.dart

--- assignment/module 2/task_2.dart
+++ assignment/module 2/task_2.dart
@@ -1,12 +1,4 @@
 import 'dart:io';
 void main(){
-  print("enter the no:");
-  var num=int.parse(stdin.readLineSync().toString());
-  
-  if(num%2==0){
-    print("$num is even");
-  }
-  else{
-    print("$num is odd");
-  }
+
 }
