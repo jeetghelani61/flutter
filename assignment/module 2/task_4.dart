b9ec24af4d79e7c94cd704bb75baf16bc1ee900c
jeetghelani61 <jeetghelani61@gmail.com>
2025-08-31 19:23:02 +0530

    Create task_4.dart

---
+++ assignment/module 2/task_4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  const double pi = 3.14159;
+
+  stdout.write("Enter the radius of the circle: ");
+  double radius = double.parse(stdin.readLineSync()!);
+
+  double area = pi * radius * radius;
+  double circumference = 2 * pi * radius;
+
+  print("Area of Circle: $area");
+  print("Circumference of Circle: $circumference");
+}
+
