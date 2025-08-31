9c3b434f26f16b194dc1d9d4abb70f7ab0c9d7d5
jeetghelani61 <jeetghelani61@gmail.com>
2025-08-31 19:03:51 +0530

    Create e.dart

---
+++ 27-08/e.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the number of terms for Fibonacci series:");
+  var n = int.parse(stdin.readLineSync().toString());
+
+  int a = 0, b = 1;
+
+  print("Fibonacci series up to $n terms:");
+  for (int i = 1; i <= n; i++) {
+    print(a);
+    int next = a + b;
+    a = b;
+    b = next;
+  }
+}
